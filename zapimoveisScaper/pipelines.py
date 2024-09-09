11e216755d30b19c351c92249ae418033cd61283
pourya90091 <pourya90091@gmail.com>
2024-09-09 21:44:18 +0330

    feat: Save items into .xlsx file

--- zapimoveisScaper/pipelines.py
+++ zapimoveisScaper/pipelines.py
@@ -6,8 +6,40 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from xlsxwriter import Workbook
+from zapimoveisScaper import settings
+from pathlib import Path
+
+EXPORT_DIR = settings.BASE_DIR / "export"
+Path(EXPORT_DIR).mkdir(parents=True, exist_ok=True) # Ensures that EXPORT_DIR exists.
 
 
 class ZapimoveisscaperPipeline:
+    def __init__(self):
+        self.workbook = None
+        self.worksheet = None
+        self.row = 1
+
     def process_item(self, item, spider):
+        self.worksheet.write(self.row, 0, item.get("competence_date"))
+        self.worksheet.write(self.row, 1, item.get("listing_id"))
+        self.worksheet.write(self.row, 2, item.get("listing_title"))
+        self.worksheet.write(self.row, 3, item.get("listing_description"))
+        self.worksheet.write(self.row, 4, item.get("property_type"))
+
+        self.row += 1
+
         return item
+
+    def open_spider(self, spider):
+        file_name = f"export.xlsx"
+        self.workbook = Workbook(f"{EXPORT_DIR}/{file_name}")
+        self.worksheet = self.workbook.add_worksheet()
+        self.worksheet.write("A1", "competence_date")
+        self.worksheet.write("B1", "listing_id")
+        self.worksheet.write("C1", "listing_title")
+        self.worksheet.write("D1", "listing_description")
+        self.worksheet.write("E1", "property_type")
+
+    def close_spider(self, spider):
+        self.workbook.close()
--- zapimoveisScaper/spiders/zapimoveis_spider.py
+++ zapimoveisScaper/spiders/zapimoveis_spider.py
@@ -1,23 +1,16 @@
 import scrapy
 from scrapy.http import Response
+from scrapy_playwright.page import PageMethod
 from pathlib import Path
 from zapimoveisScaper import settings
 import gzip
 import shutil
+import re
+from datetime import datetime
 
 
 TEMP_DIR = settings.BASE_DIR / "temp"
-
-def handle_dir(path: str) -> None:
-    """Ensures that the directory exists.
-
-    Parameters:
-        path: Path to a directory.
-    """
-
-    Path(path).mkdir(parents=True, exist_ok=True)
-
-handle_dir(TEMP_DIR)
+Path(TEMP_DIR).mkdir(parents=True, exist_ok=True) # Ensures that TEMP_DIR exists.
 
 def extract_gz(filepath):
     with gzip.open(filepath, 'rb') as f_in:
@@ -44,11 +37,12 @@ class ZapimoveisSpider(scrapy.Spider):
     namespaces = [
         ("x", "http://www.sitemaps.org/schemas/sitemap/0.9")
     ]
-    base_url = "https://www.zapimoveis.com.br/"
+    base_url = "https://www.zapimoveis.com.br"
 
     def start_requests(self):
         urls = [
-            "https://www.zapimoveis.com.br/sitemap_development_resultpage_index.xml"
+            f"{self.base_url}/sitemap_used_resultpage_streets_index.xml",
+            # f"{self.base_url}/sitemap_development_resultpage_index.xml"
         ]
 
         for url in urls:
@@ -66,12 +60,67 @@ class ZapimoveisSpider(scrapy.Spider):
         sitemap = extract_gz(path)
         local_file_url = f"file:///{sitemap}"
 
-        yield scrapy.Request(url=local_file_url, callback=self.sitemap_handler, meta={"playwright": True})
+        yield scrapy.Request(url=local_file_url, callback=self.sitemap_handler)
 
     def sitemap_handler(self, response: Response):
         pages = response.xpath("//x:loc/text()", namespaces=self.namespaces).getall()
 
-        yield from response.follow_all(pages, callback=self.page_handler, meta={"playwright": True})
+        yield from response.follow_all(pages, callback=self.page_handler, meta={
+            "playwright": True,
+            "playwright_page_methods": [
+                PageMethod("evaluate", "document.body.style.zoom = '1%';"), # Zoom out the page (force the page to load without scrolling down)
+                PageMethod("wait_for_load_state", "networkidle"), # Wait until the network is idle (all network requests are done)
+            ]})
 
     def page_handler(self, response: Response):
-        property_xpath = "//div[@data-type='STANDARD']"
+        properties = response.xpath("//div[@class='listing-wrapper__content']/div[@data-position or @data-type]//a[@href]/@href").extract()
+
+        # yield from response.follow_all(properties, callback=self.property_handler, meta={"playwright": True})
+        yield from response.follow_all(properties, callback=self.property_handler)
+
+    def property_handler(self, response: Response):
+        def remove_whitespaces(text):
+            return text.strip() if text else None
+
+        def get_property_type():
+            breadcrumb = response.xpath("//ol[contains(@class, 'breadcrumb')]/li[1]/a/text()").get()
+            if "Apartamentos" in breadcrumb:
+                property_type = "Apartment"
+            else:
+                property_type = None
+
+            return property_type
+
+        yield {
+            "competence_date": datetime.now().strftime("%Y-%m-%d"),
+            "listing_id": re.search(r"id-(\d+)/$", response.url).group(1),
+            "listing_title": remove_whitespaces(response.xpath("//h1[contains(@class, 'description__title')]/text()").get()),
+            "listing_description": remove_whitespaces(response.xpath("//p[@data-testid='description-content']/text()").get()),
+            "property_type": get_property_type(),
+            "listing_type": None,
+            "reference_market": None,
+            "location_description": None,
+            "location_region": None,
+            "location_province": None,
+            "location_city": None,
+            "location_zip": None,
+            "locaiton_neighborhood": None,
+            "location_street": None,
+            "location_street_n": None,
+            "location_lon": None,
+            "location_lat": None,
+            "area_unit": None,
+            "area_value": None,
+            "bedrooms": None,
+            "bathrooms": None,
+            "floor": None,
+            "total_floors": None,
+            "amenities_list": None,
+            "listing_date": None,
+            "listing_status": None,
+            "agent_id": None,
+            "agent_url": None,
+            "price": None,
+            "imageurl": None,
+            "itemurl": None,
+        }
